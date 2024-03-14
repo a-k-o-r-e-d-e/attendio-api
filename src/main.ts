14e2dea56579456d240ab8f1ed6f7a9b0c809103
Korede <adeyanju.akorede1@gmail.com>
2024-03-14 12:14:26 +0100

    chore: setup basic helmet and cors protection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import EnvVars from './constants/EnvVars';
 import { useRequestLogging } from './middlewares/request_logging.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
   app.setGlobalPrefix(EnvVars.BASE_URL);
   useRequestLogging(app);
   await app.listen(EnvVars.Port);
