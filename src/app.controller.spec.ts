42b998d75d64f55688c709fce1fc8af4f2c1b90f
Korede <adeyanju.akorede1@gmail.com>
2024-03-20 08:28:24 +0100

    test: Add Tests for App Controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -4,17 +4,35 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let service: AppService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        {
+          provide: AppService,
+          useValue: {
+            getHello: jest.fn().mockReturnValue('Hello World!'), // Mocking getHello method
+          },
+        },
+      ],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appController = module.get<AppController>(AppController);
+    service = module.get<AppService>(AppService);
+  });
+
+  it('should be defined', () => {
+    expect(appController).toBeDefined();
   });
 
   describe('root', () => {
+    it('should call appService.getHello', () => {
+      appController.getHello();
+      expect(service.getHello).toHaveBeenCalled();
+    });
+
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
